eb8a5256a2a5dbcea8f0d0764e0c542844fabb15
MahmoudAbojahel <mahmoudabujahel@gmail.com>
2022-10-19 21:58:26 +0300

    init

---
+++ WCF_Installation_Sample/CalculatorWindowsService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_Installation_Sample
+{
+    public class CalculatorWindowsService : ServiceBase
+    {
+        public ServiceHost serviceHost = null;
+        public CalculatorWindowsService()
+        {
+            // Name the Windows Service
+            ServiceName = "WCF_Installation_Sample";
+        }
+
+        public static void Main()
+        {
+            ServiceBase.Run(new CalculatorWindowsService());
+        }
+
+
+        // Start the Windows service.
+        protected override void OnStart(string[] args)
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+
+            // Create a ServiceHost for the CalculatorService type and 
+            // provide the base address.
+            serviceHost = new ServiceHost(typeof(CalculatorService));
+
+            // Open the ServiceHostBase to create listeners and start 
+            // listening for messages.
+            serviceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+            }
+        }
+    }
+}
